f1de84e18cf41162666ad0af7d4e138d49c43e8f
variable17 <vishal.vis17@gmail.com>
2019-01-11 17:53:44 +0530

    changes done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(name: 'Luke', password: 'unbreakable')
+user1 = User.create(name: 'Luke', password: 'unbreakable')
+
+user2 = User.create(name: 'SkyWalker', password: 'millenium-f')
+
 
 poodle = Breed.create(name: 'Poodle')
 bulldog = Breed.create(name: 'Bulldog')
@@ -14,15 +17,27 @@ pug = Breed.create(name: 'Pug')
 
 poodle_dog = ['Biscuit', 'Barney', 'Bingo', 'Chewie']
 poodle_dog.each do |name|
-  Dog.create(name: name, age: 5, image_url: 'https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwi07-e1z-XfAhUJeisKHVb2CskQjRx6BAgBEAU&url=https%3A%2F%2Fdogtime.com%2Fdog-breeds%2Fpoodle&psig=AOvVaw32FCGuYplXnaxulbip5Swp&ust=1547292377868292', user_id: user.id, breed_id: poodle.id)
+  Dog.create(name: name, age: 5, image_url: 'https://cdn2-www.dogtime.com/assets/uploads/gallery/poodle-dog-breed-pictures/elegence-1.jpg', user_id: user1.id, breed_id: poodle.id)
 end
 
 bulldog_dog = ['Archer', 'Bolt', 'Blitz', 'Gunner']
 bulldog_dog.each do |name|
-  Dog.create(name: name, user_id: user.id, age: 6, image_url: 'https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwiHpMvQz-XfAhUWVH0KHQk0BLkQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBulldog&psig=AOvVaw12zVIKtCpVM3tXnGzmE4-u&ust=1547292434013409', breed_id: bulldog.id)
+  Dog.create(name: name, user_id: user1.id, age: 6, image_url: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/CH_Buck_and_Sons_Evita_Peron.jpg', breed_id: bulldog.id)
 end
 
 pug_dog = ['Elsa', 'Ava', 'Amilie', 'Frida']
 pug_dog.each do |name|
-  Dog.create(name: name, user_id: user.id, age: 7, image_url: 'https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwi538vtz-XfAhXOfCsKHfEcB-0QjRx6BAgBEAU&url=https%3A%2F%2Fwww.akc.org%2Fdog-breeds%2Fpug%2F&psig=AOvVaw1iy1BBHN74L1j3POkjSYOF&ust=1547292494926100', breed_id: pug.id)
+  Dog.create(name: name, user_id: user1.id, age: 7, image_url: 'https://cdn1-www.dogtime.com/assets/uploads/gallery/pug-dog-breed-pictures/3-sidesitting.jpg', breed_id: pug.id)
 end
+
+
+poodle2_dog = ['Bella', 'Fifi', 'Harper']
+poodle2_dog.each do |name|
+  Dog.create(name: name, age: 5, image_url: 'https://cdn2-www.dogtime.com/assets/uploads/gallery/poodle-dog-breed-pictures/elegence-1.jpg', user_id: user2.id, breed_id: poodle.id)
+end
+
+bulldog2_dog = ['Buzz', 'K-9', 'Nyx']
+bulldog2_dog.each do |name|
+  Dog.create(name: name, user_id: user2.id, age: 6, image_url: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/CH_Buck_and_Sons_Evita_Peron.jpg', breed_id: bulldog.id)
+end
+
